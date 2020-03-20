fdc8176f8a223ee75b05962c34f2708ec556e878
Luigi Profeti <luigiprofeti@gmail.com>
2020-03-20 11:39:09 -0300

    trocada a mensagem de tenha um bom dia

--- teste/Program.cs
+++ teste/Program.cs
@@ -9,8 +9,8 @@ namespace teste {
         static void Main(string[] args) {
 
             Console.WriteLine("Hello World");
-            Console.WriteLine("Bom dia!");
             Console.WriteLine("Como você está hoje?");
+            Console.WriteLine("Tenha um bom dia!");
 
             Console.ReadLine();
         }
